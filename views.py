33de63b169ebe9bc3255f33742301ee0b52fd31e
akila <103122735+akilkookie@users.noreply.github.com>
2023-03-23 11:07:27 +0530

    Update views.py

--- views.py
+++ views.py
@@ -10,38 +10,6 @@ from .models import CustomUser
 
 from accounts.serializers import CreateUserSerializer, UpdateUserSerializer, LoginSerializer
 
-
-# # Create your views here.
-# @api_view(["POST"])
-# @permission_classes([AllowAny])
-# def Register_Users(request):
-#     try:
-#         data = []
-#         serializer = RegistrationSerializer(data=request.data)
-#         if serializer.is_valid():
-#             account = serializer.save()
-#             account.is_active = True
-#             account.save()
-#             token = Token.objects.get_or_create(user=account)[0].key
-#             data["message"] = "user registered successfully"
-#             data["email"] = account.email
-#             data["username"] = account.username
-#             data["token"] = token
-#
-#         else:
-#             data = serializer.errors
-#
-#
-#         return Response(data)
-#     except IntegrityError as e:
-#         account=User.objects.get(username='')
-#         account.delete()
-#         raise ValidationError({"400": f'{str(e)}'})
-#
-#     except KeyError as e:
-#         print(e)
-#         raise ValidationError({"400": f'Field {str(e)} missing'})
-
 class CreateUserAPI(CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CreateUserSerializer
